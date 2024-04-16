d2b398b18b0a70f2cca9603c0bca6dfabb58b62f
mikle <mikle>
2024-04-16 19:33:51 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            var process = new char[] {'-', '\\', '|', '/'};
+            var i = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                    i = i > 3 ? 0 : i;
+                    System.out.print("\r load: " + process[i]);
+                    Thread.sleep(500);
+                    i++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
