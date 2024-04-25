c8feb1f2a39c61373f70947d82d3951954f4dc00
mikle <mikle>
2024-04-25 18:34:38 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/GetFile.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.function.Predicate;
+
+public class GetFile {
+    private final File file;
+
+    public GetFile(final File file) {
+        this.file = file;
+    }
+
+    public String getContent() throws IOException {
+        return content(x -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return content(x -> x < 0x80);
+    }
+
+    public String content(Predicate<Character> filter) throws IOException {
+        try (InputStream input = new FileInputStream(file)) {
+            String output = "";
+            int data;
+            while ((data = input.read(new byte[1024], 0, new byte[1024].length)) != -1) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,11 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class ParseFile {
+    private final File file;
+
+    public ParseFile(final File file) {
+        this.file = file;
+    }
+}
