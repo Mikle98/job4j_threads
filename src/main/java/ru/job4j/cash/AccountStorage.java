91b3b5bceb7f808c8b73ed3a745e286c3c6988e4
mikle <mikle>
2024-04-30 09:35:38 +0300

    3. Денежные переводы AccountStorage [#1104 #493120]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -4,6 +4,7 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Optional;
 
 @ThreadSafe
@@ -12,19 +13,11 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        var rsl =  accounts.containsKey(account.id());
-        if (!rsl) {
-            accounts.put(account.id(), account);
-        }
-        return accounts.containsValue(account);
+        return Objects.isNull(accounts.putIfAbsent(account.id(), account));
     }
 
     public synchronized boolean update(Account account) {
-        var rsl = accounts.containsKey(account.id());
-        if (rsl) {
-            accounts.put(account.id(), account);
-        }
-        return rsl;
+        return Objects.isNull(accounts.replace(account.id(), account));
     }
 
     public synchronized void delete(int id) {
@@ -36,10 +29,16 @@ public class AccountStorage {
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        var rsl = getById(fromId).get().amount() - amount >= 0;
-        if (rsl) {
-            update(new Account(fromId, getById(fromId).get().amount() - amount));
-            update(new Account(toId, getById(toId).get().amount() + amount));
+        var rsl = false;
+        var fromAccountOptional = getById(fromId);
+        var toAccountOptional = getById(toId);
+        if (fromAccountOptional.isPresent()
+                && toAccountOptional.isPresent()) {
+            rsl = fromAccountOptional.get().amount() - amount >= 0;
+            if (rsl) {
+                update(new Account(fromId, fromAccountOptional.get().amount() - amount));
+                update(new Account(toId, toAccountOptional.get().amount() + amount));
+            }
         }
         return rsl;
     }
