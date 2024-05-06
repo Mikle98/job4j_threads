b2add6ae7a72960ce55aef72f2f45871843388a5
mikle <mikle>
2024-05-06 18:33:46 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/EmailNotification.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newCachedThreadPool();
+
+    public void emailTo(User user) {
+        pool.submit(
+                () -> {
+                    var subject = String.format("Notification %s to email %s",
+                                                        user.username(),
+                                                        user.email());
+                    var body = String.format("Add a new event to %s",
+                                                    user.username());
+                    send(subject, body, user.email());
+                }
+        );
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+        System.out.println("Send msg " + email);
+    }
+
+    public static void main(String[] args) {
+        EmailNotification notification = new EmailNotification();
+        User user = new User("test", "123@123.12");
+        notification.emailTo(user);
+        notification.close();
+    }
+}
---
+++ src/main/java/ru/job4j/User.java
@@ -0,0 +1,4 @@
+package ru.job4j;
+
+public record User(String username, String email) {
+}
